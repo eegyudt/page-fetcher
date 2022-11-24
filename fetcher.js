82975f533fe94a9cc04cf88a270c65ca9ad91bc5
Eszter Egyud <eszter.egyud@telus.com>
2022-11-23 21:34:54 -0700

    implemented page fetcher

---
+++ fetcher.js
@@ -0,0 +1,27 @@
+//page fetcher
+const request = require('request');
+const fs = require('fs');
+
+const args = process.argv;
+
+let page = args[2];
+let fileName = args[3];
+
+request(page, function(error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  };
+
+  fs.writeFile(fileName, body, function(error) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    console.log(`Downloaded and saved ${body.length} bytes to ${fileName}`);
+  });
+});
+
+
+
